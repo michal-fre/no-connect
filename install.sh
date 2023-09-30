9b6f62bd3f6745b78b113f5dadc34135f7307201
Michal <michal@fresel.at>
2023-09-30 14:53:49 +0200

    :)

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 #
 
-TEMP_INSTALL=$(mktemp)
-curl -o $TEMP_INSTALL https://raw.githubusercontent.com/michal-fre/no-connect/master/install.sh
+
+
+if [ ! -d /usr/local/sbin/no-connect ] ; then  
+	mkdir /usr/local/sbin/no-connect
+fi
+if [ ! -d /var/spool/no-connect ] ; then  
+	mkdir /var/spool/no-connect
+fi
+if [ ! -d /etc/no-connect ] ; then  
+	mkdir /etc/no-connect
+fi
+
+echo "get the files"
+curl -o /var/spool/no-connect/install.sh https://raw.githubusercontent.com/michal-fre/no-connect/master/install.sh
+chmod +x /var/spool/no-connect/install.sh
+if [ ! -f /usr/local/sbin/no-connect/install.sh ]; then 
+	mv -fu /var/spool/no-connect/install.sh /usr/local/sbin/no-connect/install.sh
+fi
+
+curl -o /etc/no-connect/ipset-allow.conf https://raw.githubusercontent.com/michal-fre/no-connect/master/ipset-allow.conf
+curl -o /etc/no-connect/ipset-allow-custom.conf https://raw.githubusercontent.com/michal-fre/no-connect/master/ipset-allow-custom.conf
+curl -o /etc/no-connect/ipset-block.conf https://raw.githubusercontent.com/michal-fre/no-connect/master/ipset-block.conf
+curl -o /etc/no-connect/ipset-block-custom.conf https://raw.githubusercontent.com/michal-fre/no-connect/master/ipset-block-custom.conf
+
